ea9295fa6156be8cb6530e7c4d9e63a43d315c0b
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-24 20:14:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if current_user
+     redirect_to root_path
+   elsif params[:name] && !params[:name].empty?
+     session[:name] = params[:name]
+     redirect_to root_path
+   else
+     redirect_to login_path
+   end
 
   end
 
